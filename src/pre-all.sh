10d34460642c634e49d160d5becc3277f12a57e8
bud42 <bdboyd42@gmail.com>
2024-07-10 19:12:20 -0400

    aparc dk40 all sessions on all subjects

---
+++ src/pre-all.sh
@@ -0,0 +1,20 @@
+set -x
+
+mkdir -p /OUTPUTS/DATA/SUBJECTS
+
+cd /INPUTS
+for i in *;do
+    cd /INPUTS/$i
+    for j in *;do
+
+        # Copy from inputs to outputs
+        cp -r /INPUTS/$i/$j/*/SUBJ /OUTPUTS/DATA/SUBJECTS/$j
+
+        # Append to subject list
+        echo $j >> /OUTPUTS/subjects.txt
+
+    done
+done
+
+# Copy in fsaverage
+cp -r /opt/ext/fsaverage /OUTPUTS/DATA/SUBJECTS
