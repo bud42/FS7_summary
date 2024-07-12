56783c667dc0eaf51cedd4774b0b27023f0aa3ae
bud42 <bdboyd42@gmail.com>
2024-07-12 08:32:54 -0400

    updates

--- src/pre-all.sh
+++ src/pre-all.sh
@@ -3,18 +3,20 @@ set -x
 mkdir -p /OUTPUTS/DATA/SUBJECTS
 
 cd /INPUTS
+
+# Iterate subjects
 for i in *;do
+
+    # Iterate subdirs (sessions)
     cd /INPUTS/$i
+
     for j in *;do
 
-        # Copy from inputs to outputs
-        cp -r /INPUTS/$i/$j/*/SUBJ /OUTPUTS/DATA/SUBJECTS/$j
+        # Copy from inputs to outputs concat subject and subdir name
+        cp -r /INPUTS/$i/$j /OUTPUTS/DATA/SUBJECTS/${i}_${j}
 
         # Append to subject list
-        echo $j >> /OUTPUTS/subjects.txt
-
-    done
-done
-
+        echo ${i}_${j} >> /OUTPUTS/subjects.txt
+ 
 # Copy in fsaverage
 cp -r /opt/ext/fsaverage /OUTPUTS/DATA/SUBJECTS
