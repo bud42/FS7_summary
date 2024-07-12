e56d0022edc131e344536dedba825cf633d5e516
bud42 <bdboyd42@gmail.com>
2024-07-12 10:11:54 -0400

    updates for dax 2.13.22

--- src/pre-all.sh
+++ src/pre-all.sh
@@ -4,19 +4,17 @@ mkdir -p /OUTPUTS/DATA/SUBJECTS
 
 cd /INPUTS
 
-# Iterate subjects
 for i in *;do
 
-    # Iterate subdirs (sessions)
     cd /INPUTS/$i
 
     for j in *;do
 
-        # Copy from inputs to outputs concat subject and subdir name
-        cp -r /INPUTS/$i/$j /OUTPUTS/DATA/SUBJECTS/${i}_${j}
+        # Copy from inputs to outputs
+        cp -r /INPUTS/$i/$j /OUTPUTS/DATA/SUBJECTS/${j}
 
         # Append to subject list
-        echo ${i}_${j} >> /OUTPUTS/subjects.txt
- 
+        echo ${j} >> /OUTPUTS/subjects.txt
+
 # Copy in fsaverage
 cp -r /opt/ext/fsaverage /OUTPUTS/DATA/SUBJECTS
